e136438c3a7d45ed6f082f8046979bfbc401f88e
RafaelMartinSerrano <114928915+RafaelMartinSerrano@users.noreply.github.com>
2023-06-07 14:37:27 +0200

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,18 +1,8 @@
 # Importar los módulos necesarios
 from sdg.open_sdg import open_sdg_build
-from sdg.outputs import OutputDocumentationService
 
 # Llamar a la función open_sdg_build
 open_sdg_build(config='config_data.yml')
-
-# Crear una instancia de OutputDocumentationService
-documentation_service = OutputDocumentationService()
-
-# Generar la documentación
-documentation_service.generate_documentation()
-
-# Obtener el MetadataReportService desde documentation_service
-service = documentation_service.metadata_report_service
 
 # Verificar si la clave existe en el diccionario store
 if 'SDG_CUSTODIAN_AGENCIES__GLOBAL' in service.store:
